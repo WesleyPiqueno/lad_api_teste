850ee7d7993b0a29e8d2071455ba15a190ffbd9d
WesleySampaioCunha <74968786+WesleyPiqueno@users.noreply.github.com>
2021-05-06 14:17:45 -0300

    Update BlankApplicationTests.java

--- src/test/java/com/dgc/BlankApplicationTests.java
+++ src/test/java/com/dgc/BlankApplicationTests.java
@@ -1,16 +1,41 @@
 package com.dgc;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import java.ulti.Scanner;
+private static Scanner teclado;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlankApplicationTests {
-
+punlic static void main (String []args){
+	
+	teclado = new Scanner (System.in);
+	
+	System.out.println("Cliente:);
+	String cliente = cliente.nextIntLine();
+			   
+	System.out.println("Email :");
+	String email = email.nextIntLine();
+			   
+	float cpf = teclado.nextFloat();
+	System.out.println("CPF:");		   
+	
+			    System.out.println("Cliente:" + cliente );
+			   
+			    System.out.println("Valor da Nota:"+ cpf);
+			    
+	
+	
 	@Test
 	public void contextLoads() {
+	
+	
+		
+		
+		
+		
 	}
 
+}
 }
